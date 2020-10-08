18fac3eb6c56f58a7a7fcb517e93ef7d9a2b9b2a
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-10-08 12:11:51 -0700

    added comments

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
-
+// grabs the HTML elements by the id
 displayCal = document.querySelector('#display-calorie')
 slcActivity = document.querySelector('#activity')
 minutes = document.querySelector('#workout')
 pounds = document.querySelector('#weight')
-
+// converts the weight input from pounds to kiloGrams
 function convertPounds(){
     const poundsVal = parseInt(pounds.value)
     kGrams = poundsVal * 0.45
     return kGrams
 }
+// takes the value from each input and calculate the calories burned
 function calcCalories(){
     const slcActivityVal = parseInt(slcActivity.value)
     const minutesVal = parseInt(minutes.value)
@@ -17,7 +18,7 @@ function calcCalories(){
     displayCal.innerHTML = calBurned
     console.log(calBurned)
 }
-
+// listens for an input and then calculate the calories based on the input
 slcActivity.addEventListener('input', calcCalories)
 minutes.addEventListener('input', calcCalories)
 pounds.addEventListener('input', calcCalories)
